caecc1a25770addf8b1e9244243f1aaec2d7feba
AndreRivas2020 <andrerivas2011@gmail.com>
2023-11-07 08:16:56 -0600

    Proyecto Final 201925506

---
+++ Models/AccountModels.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcMusicStore.Models
+{
+    using Compare = System.ComponentModel.DataAnnotations.CompareAttribute;
+
+    public class ChangePasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña actual")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "El {0} debería tener al menos {2} carácteres.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Nueva contraseña")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar nueva contraseña")]
+        [Compare("NewPassword", ErrorMessage = "la nueva contraseña y la de confirmación no son iguales.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LogOnModel
+    {
+        [Required]
+        [Display(Name = "Usuario")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [Display(Name = "Recordar?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "Usuario")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "El {0} debería tener al menos {2} carácteres.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar contraseña")]
+        [Compare("Password", ErrorMessage = "la contraseña y la de confirmación no son iguales.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Models
+{
+[Bind(Exclude = "OrderId")]
+public partial class Order
+{
+[ScaffoldColumn(false)]
+public int OrderId { get; set; }
+[ScaffoldColumn(false)]
+public System.DateTime OrderDate { get; set; }
+[ScaffoldColumn(false)]
+public string Username { get; set; }
+[Required(ErrorMessage = "Primer nombre es requerido")]
+[DisplayName("Primer nombre")] [StringLength(160)]
+public string FirstName { get; set; }
+[Required(ErrorMessage = "Apellido es requerido")]
+[DisplayName("Apellido")]
+[StringLength(160)]
+public string LastName { get; set; }
+[Required(ErrorMessage = "Dirección es requerida")]
+[StringLength(70)]
+public string Direccion { get; set; }
+[Required(ErrorMessage = "Ciudad es requerida")]
+[StringLength(40)]
+public string Ciudad { get; set; }
+[Required(ErrorMessage = "Estado es requerido")]
+[StringLength(40)]
+public string Estado { get; set; }
+[Required(ErrorMessage = "Código postal es requerido")]
+[DisplayName("Código Postal")]
+[StringLength(10)]
+public string Postal { get; set; }
+[Required(ErrorMessage = "País es requerido")]
+[StringLength(40)]
+public string País { get; set; }
+[Required(ErrorMessage = "Teléfono es requerido")]
+[StringLength(24)]
+public string Teléfono { get; set; }
+[Required(ErrorMessage = "Correo electrónico es requerido")]
+[DisplayName("Correo electrónico")]
+[RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+ErrorMessage = "Correo no es válido.")]
+[DataType(DataType.EmailAddress)]
+public string Email { get; set; }
+[ScaffoldColumn(false)]
+public decimal Total { get; set; }
+public List<OrderDetail> OrderDetails { get; set; }
+}
+}
